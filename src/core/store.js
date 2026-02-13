93afd306b8104f1a8273c56ec881118fd9d82fde
Nilesh Roy <nileshroy404@gmail.com>
2026-02-13 16:04:41 +0530

    add global state management (initial)

---
+++ src/core/store.js
@@ -0,0 +1,61 @@
+import { save, load } from "../services/storage";
+
+export class Store {
+   constructor () {
+
+      this.state = {
+          notes: [],
+          tabs: [],
+          activeNoteId: null
+      }
+
+      this.listners = []
+
+      const savedNote = load()
+      if(savedNote){ this.state = savedNote }
+   }
+
+   subscribe(renderer){
+      this.listners.push(renderer);
+   }
+
+   notify(){
+      this.listners.forEach(render => {
+         render()
+      })
+   }
+}
+
+
+
+
+// import { saveNotes, loadNotes } from "../services/storage.js"
+// import { noteModel } from "../models/note.model.js"
+
+// export class Store {
+//    constructor() {
+//       this.notes = loadNotes()
+//       this.listeners = []
+//    }
+
+//    saveNotes() {
+//       saveNotes(this.notes)
+//    }
+
+   // createNote(title, content) {
+   //    const newNote = noteModel(title, content)
+   //    this.notes.unshift(newNote)
+   //    this.saveNotes()
+   //    console.log(this.notes)
+   // }
+
+   // updateNote(id, updates) {
+   //    this.notes = this.notes.map(note => {
+   //       note.id === id ? { ...note, ...updates } : note
+   //    });
+   // }
+
+   // deleteNote(id) {
+   //    this.notes = this.notes.filter(note => note.id !== id)
+   // }
+// }
