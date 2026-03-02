2aad9bf383409616f14938ace3d77ed2d38fecee
Nilesh Roy <nileshroy404@gmail.com>
2026-03-02 21:56:00 +0530

    attach event for note creation, open in tab, open note, tab closing

--- src/ui/render.js
+++ src/ui/render.js
@@ -7,43 +7,35 @@ import { tabItem } from "./components";
 
 
 export function renderApp(state, noteServices, tabManager) {
-
   const root = document.getElementById("app")
-
-
   root.innerHTML = ""
   root.append(aside(), workspace())
 
   // list all note in aside section
   const noteList = document.getElementById('note-list')
-
   state.notes.forEach(note => {
     noteList.append(noteItem(note))
   });
 
   // tabs
   const tabSpace = document.querySelector("#tab-space")
-
   state.tabs.forEach(tab => {
     const note = state.notes.find(n => n.id === tab.history[tab.historyIndex])
     tabSpace.append(tabItem(note, tab.id))
   })
-  attachEvent(state, noteServices, tabManager)
+  attachEventTab(state, noteServices, tabManager)
 }
 
 
+
 // attach event after render updates
-function attachEvent(state, services, tabManager) {
-
-
+function attachEventTab(state, services, tabManager) {
   // Event one - create note + in new tab
   const createNoteBtn = document.querySelector("#create-note")
-
   createNoteBtn.addEventListener('click', () => {
     const noteId = services.createNote()
     tabManager.openNewTab(noteId)
   })
-
 
   // Event two - close tab
   const tabSpace = document.querySelector("#tab-space")
@@ -55,25 +47,32 @@ function attachEvent(state, services, tabManager) {
     tabManager.closeTab(tabId)
   })
 
-  // Event three
-  // const listItems = document.querySelector("#note-list")
+  // Event three - click note to open
+  const listItems = document.querySelector("#note-list")
+  listItems.addEventListener("click", (e) => {
+    const item = e.target.closest(".note-ref-item")
+    if (!item) return
 
-  // listItems.addEventListener("click", (e) => {
-  //   const item = e.target.closest(".note-ref-item")
-  //   if (!item) return
+    const noteId = item.dataset.id
 
-  //   const noteId = item.dataset.id
+    if (state.activeTabId === null) {
+      tabManager.openNewTab(noteId)
+    }
+    else if(e.ctrlKey){
+      tabManager.openNewTab(noteId)
+    }
+    else {
+      tabManager.openInTab(noteId)
+    }
+  })
 
-  //   if (state.activeTabId === null) {
-  //     tabManager.openNewTab(noteId)
-  //   }
-  //   else {
-  //     tabManager.openInTab(noteId)
-  //   }
-  // })
+
+  // Event three - select tab
+
+
+  // Event four - back/forward
 
 }
-
 
 
 
